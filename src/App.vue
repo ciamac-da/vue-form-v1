14f36b51d6409969437e1c852ec25305aebb873a
ciamac-da <ciamac.davoudi@gmail.com>
2021-11-15 21:09:53 +0100

    Verified by Marti4n 1.0.2 form setup

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
+<SignupForm />
 </template>
 
 <script>
+import SignupForm from "./components/SignupForm"
 
 export default {
   name: 'App',
-  components: {}
+  components: { SignupForm}
 }
 </script>
 
 <style>
-#app {
+body {
   font-family: 'Lato', sans-serif;
   font-family: 'Rubik', sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -18,5 +20,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background: #f2f2f2;
 }
 </style>
