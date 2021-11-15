e15c0d5261cb308a8f460b350be17d6ac15534b5
ciamac-da <ciamac.davoudi@gmail.com>
2021-11-15 20:04:14 +0100

    Verified by Marti4n 1.0.1

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Lato', sans-serif;
+  font-family: 'Rubik', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
