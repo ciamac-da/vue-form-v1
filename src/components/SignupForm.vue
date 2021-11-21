69ada76e65ae753a77f02d921eb4ae057641f115
ciamac-da <ciamac.davoudi@gmail.com>
2021-11-21 08:57:34 +0100

    Verified by Marti4n 1.0.6 keyboard events & modifiers

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -16,7 +16,7 @@
 <label>Skills:</label>
 <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
 <div v-for="skill in skills" :key="skill" class="pill">
-{{ skill}}
+  <span @click="deleteSkill(skill)"> {{ skill }} </span>  
 </div>
 
 <div class="terms">
@@ -48,6 +48,11 @@ methods: {
       }
       this.tempSkill = ""
     }
+  },
+  deleteSkill(skill) {
+    this.skills = this.skills.filter((item) => {
+      return skill !== item
+    })
   }
 },
 }
@@ -87,5 +92,17 @@ input[type="checkbox"] {
   margin: 0 10px 0 0;
   position: relative;
   top: 2px;
+}
+.pill {
+  display: inline-block;
+  padding: 6px 12px;
+  margin: 20px 10px 0 0;
+  background: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
 }
 </style>
